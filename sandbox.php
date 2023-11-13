ad45d752d64ecb1ee7ba6915a9cc8f5be93b12f3
Tim Dominguez - GIAA <tim.dominguez@guamairport.net>
2023-11-13 11:33:28 +1000

    remove sandbox.php

--- sandbox.php
+++ sandbox.php
@@ -2,5 +2,29 @@
 
 include 'functions.php';
 
+sql_connect();
+
+// $dirPath contain path to directory whose files are to be listed 
+$dirPath = 'C:\xampp\htdocs\sandbox';
+
+$files = scandir($dirPath);  
+
+foreach ($files as $file) {
+    $filePath = $dirPath . '/' . $file;
+    if (is_file($filePath)) {
+        
+        //remove w2.pdf
+        $file = substr($file,0,-6);
+
+        //split names into array
+        $file = explode(" ",$file);
+
+        //output names
+        foreach ($file as $filename){
+            echo $filename . " ";
+        }
+        echo "<br>";
+    }
+}
 
 ?>
